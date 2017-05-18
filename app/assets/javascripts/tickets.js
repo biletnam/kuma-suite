977b4801c3e5266a68ce80c365e0ae043ab39392
Jason <jzntam@gmail.com>
2017-05-18 00:37:26 -0700

    Add Ajax call for tickets index

--- app/assets/javascripts/tickets.js
+++ app/assets/javascripts/tickets.js
@@ -5,3 +5,35 @@ $(document).ready(function() {
 $("#ticket-table").tablesorter();
 
 } );
+
+$(document).ready(function() {
+  function updateTicket(id, ticketParams) {
+    $.ajax({
+      url: '/tickets/' + id,
+      data: { ticket: ticketParams },
+      method: 'PATCH',
+      success: function(response) {
+        console.log(response.responseText)
+      },
+      error: function(response) {
+        console.log(response.responseText)
+      }
+    })
+  }
+
+  $('.ticket_row button').on('click', function(event) {
+    var ticket_row = $(this).parents('.ticket_row')
+
+    var ticket_id = ticket_row.data().id
+    // var ticket_flag = ticket_row.find('.ticket_flag a.text').text()
+    var ticket_status = ticket_row.find('.ticket_status a.text').text()
+    var ticket_rep = ticket_row.find('.ticket_rep a.text').text()
+
+    updateParams = {
+      status: ticket_status,
+      rep: ticket_rep
+    }
+
+    updateTicket(ticket_id, updateParams)
+  })
+})
