0a3cd1971b4bf28efb4995c8184df47d45a7a2c6
Nic Tamura <nictamura@gmail.com>
2017-05-20 13:26:22 -0700

    implemented users and staff data tabs, modals for staff

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  $('#modaluserbtn').click(function(){
+    $('#modaluser').modal('show');
+  });
+  $('#modalstaffbtn').click(function(){
+    $('#modalstaff').modal('show');
+  });
+
+  // $('.ui.dropdown').dropdown();
+
+
+
+});
