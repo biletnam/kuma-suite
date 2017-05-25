3380577abbf98f955c8e22f61503213f01dd26c1
Jason <jzntam@gmail.com>
2017-05-25 09:57:11 -0700

    Refactor PaymentsController

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,30 +1,55 @@
 class PaymentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_invoice, only: [:new, :create]
 
   def new
-    @invoice = Invoice.find params[:invoice_id]
   end
 
   def create
-    customer = Stripe::Customer.create(
+    customer = create_stripe_customer
+    card_info = customer.sources.data[0]
+    card_exp = "#{card_info.exp_month}/#{card_info.exp_year}"
+    charge = create_stripe_charge(customer)
+
+    update_user(customer, card_info, card_exp)
+
+    if @invoice.update(stripe_charge_id: charge.id)
+      flash[:notice] = 'Thanks for the money lol'
+      redirect_to @invoice
+    else
+      flash[:alert] = 'Problem processing Payment'
+      redirect_to @invoice
+    end
+  end
+
+  private
+
+  def find_invoice
+    @invoice = Invoice.find(params[:invoice_id])
+  end
+
+  def create_stripe_customer
+    Stripe::Customer.create(
       description: "Customer for #{current_user.id} | #{current_user.email}",
       source: params[:stripe_token]
     )
-    card_info = customer.sources.data[0]
-    exp = "#{card_info.exp_month}/#{card_info.exp_year}"
-    current_user.update stripe_customer_id: customer.id,
-                        stripe_card_brand: card_info.brand,
-                        stripe_last4: card_info.last4,
-                        stripe_card_expiry: exp
+  end
 
-    @invoice = Invoice.find params[:invoice_id]
-    charge = Stripe::Charge.create(
+  def create_stripe_charge(customer)
+    Stripe::Charge.create(
       amount: @invoice.amount * 100,
       currency: "cad",
       customer: customer.id,
       description: "Invoice ID #{@invoice.id} payment"
-      )
-      @invoice.update stripe_charge_id: charge.id
-      redirect_to @invoice, notice: 'Thanks for the money lol'
+    )
+  end
+
+  def update_user(customer, card_info, card_exp)
+    current_user.update(
+      stripe_customer_id: customer.id,
+      stripe_card_brand: card_info.brand,
+      stripe_last4: card_info.last4,
+      stripe_card_expiry: card_exp
+    )
   end
 end
